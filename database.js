53e3522a0b35aff99f6ff028e7b8f73c44e43238
craftinguser <craftingwork94@gmail.com>
2025-02-06 18:15:32 +0530

    Revert "first commit"
    This reverts commit e70e3477d6c902426a28d318464d42aba06f7886.

--- database.js
+++
@@ -1,8 +0,0 @@
-//Go to mongodb website
-//Create a free MO cluster
-//Create a user
-//Get the connection string
-
-require("mongodb")
-
-const URI = "mongodb+srv://ritudb94:329BadMneacl3zB4@practiceproject.et8e6.mongodb.net/";
