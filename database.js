acd52fd669ae2f51c7f578f93e723e7d23964554
craftinguser <craftingwork94@gmail.com>
2025-02-06 18:21:13 +0530

    Raw file

---
+++ database.js
@@ -0,0 +1,8 @@
+//Go to mongodb website
+//Create a free MO cluster
+//Create a user
+//Get the connection string
+
+require("mongodb")
+
+const URI = "mongodb+srv://ritudb94:329BadMneacl3zB4@practiceproject.et8e6.mongodb.net/";
